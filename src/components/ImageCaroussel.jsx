92ab98dc3c9a4671bbae397d94fa618a556e3019
Pedro Castro <pedro@gruvi.tv>
2025-05-30 00:22:20 +0100

    update logo size and carousel

--- src/components/ImageCaroussel.jsx
+++ src/components/ImageCaroussel.jsx
@@ -3,52 +3,26 @@ import Slider from "react-slick";
 const ImageCarousel = ({ images }) => {
   var settings = {
     dots: true,
-    arrows: true,
+    arrows: false,
     infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
 
   return (
-    <div className="mt-8">
+    <div className="mt-8  max-w-[800px] ml-0">
       <Slider {...settings}>
         {images.map((image, index) => (
-          <div key={index} className="px-2">
-            <div className="w-full h-64 overflow-hidden flex items-center justify-center    ">
-              <img
-                src={image}
-                alt={`Slide ${index}`}
-                className="h-full w-auto object-cover"
-              />
-            </div>
+          <div key={index} className="">
+            <img
+              src={image}
+              alt={`Slide ${index}`}
+              className="h-[500px] w-auto object-contain"
+            />
           </div>
         ))}
       </Slider>
